a55c20b5f7c45cbfdaf5b90db67774ee458c2367
Maxim Volgin <maxim.volgin@me.com>
2017-10-24 22:17:10 +0200

    Initial Commit

---
+++ RxCoreNFC/RxCoreNFC.h
@@ -0,0 +1,19 @@
+//
+//  RxCoreNFC.h
+//  RxCoreNFC
+//
+//  Created by Maxim Volgin on 24/10/2017.
+//  Copyright © 2017 Maxim Volgin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxCoreNFC.
+FOUNDATION_EXPORT double RxCoreNFCVersionNumber;
+
+//! Project version string for RxCoreNFC.
+FOUNDATION_EXPORT const unsigned char RxCoreNFCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxCoreNFC/PublicHeader.h>
+
+
