f7f242a5cf2dcd7dc15da20f3b70232c1f11ef15
Maxim Volgin <maxim.volgin@me.com>
2017-10-24 22:38:57 +0200

    dependencies

---
+++ RxCoreNFC/RxCoreNFC.swift
@@ -0,0 +1,12 @@
+//
+//  RxCoreNFC.swift
+//  RxCoreNFC
+//
+//  Created by Maxim Volgin on 24/10/2017.
+//  Copyright © 2017 Maxim Volgin. All rights reserved.
+//
+
+import CoreNFC
+import RxSwift
+
+// TODO
